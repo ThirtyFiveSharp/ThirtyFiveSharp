4d9d166438547bd8dbf4ba4b21081547a4968f5e
swzhou <swing.zhou@hotmail.com>
2012-10-18 07:28:09 +0800

    [SW] #N/A set up ror skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirtyFiveSharp::Application.config.secret_token = '6df0580e85c725b5ba443efa9f2f89a955c7bea8e4db804b75eadff6120194743bbdd64a235c92ce1117c099c1de548b4c94dbd998c6d51250e0ba5238fad96d'
